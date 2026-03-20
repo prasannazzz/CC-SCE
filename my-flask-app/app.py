ca3f0204c2e2ce6036486ce2b35289e71e079c60
prasanna <proplayer14102004@gmail.com>
2026-03-20 21:27:58 +0530

    Initial commit

---
+++ my-flask-app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    hostname = os.environ.get('HOSTNAME', 'unknown')
+    return f'''
+    <h1>Hello World!</h1>
+    <p>Running on Kubernetes</p>
+    <p>Pod: {hostname}</p>
+    '''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
